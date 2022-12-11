9fc8e9a2e12964e8983ff6cf18c556476b523519
metraf <firsov.tyoma@gmail.com>
2022-12-11 18:02:20 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
-
+import time
 from scraper import Scraper
 
 if __name__ == '__main__':
-    dfs = [pd.read_html(f'https://de.wikipedia.org/wiki/SDAX')[2], pd.read_html(f'https://de.wikipedia.org/wiki/MDAX')[1], pd.read_html(f'https://de.wikipedia.org/wiki/TECDAX')[5], pd.read_html(f'https://de.wikipedia.org/wiki/DAX')[1]]
+    start_time = time.time()
+    dfs = [pd.read_html(f'https://de.wikipedia.org/wiki/MDAX')[1], pd.read_html(f'https://de.wikipedia.org/wiki/TECDAX')[5], pd.read_html(f'https://de.wikipedia.org/wiki/DAX')[1], pd.read_html(f'https://de.wikipedia.org/wiki/SDAX')[2]]
     s = Scraper()
     for df in dfs:
         for v in df["Name"].values:
             print(f"Scraping {v}")
             s.download_company(v)
     s.close()
+    print("--- %s seconds ---" % (time.time() - start_time))
--- scraper.py
+++ scraper.py
@@ -9,9 +9,12 @@ import time
 
 class Scraper:
     def __init__(self):
+        self.init_server()
+
+    def init_server(self):
         self.url = "https://www.unternehmensregister.de/ureg/"
         options = webdriver.ChromeOptions()
-        prefs = {"download.default_directory": "/tmp/reports"}
+        prefs = {"download.default_directory": "/tmp/reports/daxes"}
         options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.get(self.url)
@@ -24,44 +27,36 @@ class Scraper:
         search_input.clear()
         search_input.send_keys(company_name)
         delay = 0.3
+        if self.wait_and_click(By.ID, "globalSearchForm:btnExecuteSearchOld"): return
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "Registerinformationen des Registergerichts"): return
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "Registerinformationen anzeigen"): return
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "AD"): return
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "Dokumentenkorb ansehen"): return
         time.sleep(delay)
-        self.driver.find_element(By.ID, "globalSearchForm:btnExecuteSearchOld").click()
+        self.driver.find_elements(By.CSS_SELECTOR, "input.btn.btn-green")[1].click()
+        time.sleep(delay)
+        self.driver.find_elements(By.CSS_SELECTOR, "input.btn.btn-green")[1].click()
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "Zum Dokumentenkorb"): return
+        if self.wait_and_click(By.PARTIAL_LINK_TEXT, "Dokument abrufen"): return
+        if self.wait_and_click(By.CSS_SELECTOR, "a.logo"): return
+
+        time.sleep(delay)
+
+    def wait_and_click(self, by, value, delay=10):
+        err = False
         try:
             WebDriverWait(self.driver, delay).until(
-                EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Registerinformationen des Registergerichts")))
+                EC.presence_of_element_located((by, value)))
         except TimeoutException:
             print("Timed out waiting for page to load")
+            err = True
         finally:
-            print("Page loaded")
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Registerinformationen des Registergerichts").click()
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        time.sleep(delay)
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Registerinformationen anzeigen").click()
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        time.sleep(delay)
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "AD").click()
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        time.sleep(delay)
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Dokumentenkorb ansehen").click()
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        time.sleep(delay)
-        self.driver.find_elements(By.CSS_SELECTOR, "input.btn.btn-green")[1].click()
-        time.sleep(delay)
-        self.driver.find_elements(By.CSS_SELECTOR, "input.btn.btn-green")[1].click()
-        time.sleep(delay)
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Zum Dokumentenkorb").click()
-        time.sleep(delay)
-        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Dokument abrufen").click()
-        time.sleep(delay)
-        try:
-            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.logo")))
-        except TimeoutException:
-            print("Timed out waiting for page to load")
-        finally:
-            print("Page loaded")
-        self.driver.find_element(By.CSS_SELECTOR, "a.logo").click()
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        time.sleep(delay)
+            if err:
+                self.close()
+                self.init_server()
+            else:
+                self.driver.find_element(by, value).click()
+            return err
 
     def close(self):
         self.driver.close()
